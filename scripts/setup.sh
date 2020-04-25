2792194a7af51fe6d581adc6abafa5b8c456d1fb
Troy Chaplin <troy.chaplin@gmail.com>
2020-04-25 09:06:30 -0400

    Updated correct setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,11 +8,12 @@ mkdir ./wp-content/uploads
 chmod 777 ./wp-content/uploads
 
 # Build symlinks on install
-setup_path=$(pwd)
-ln -s $setup_path/app/stable ./wp
-ln -s $setup_path/config/.htaccess-standard ./.htaccess
-ln -s $setup_path/config/wp-cli.yml ./wp-cli.yml
-ln -s $setup_path/config/wp-env.php ./wp-env.php
+setup_basepath=`pwd`
+
+ln -s $setup_basepath/app/stable ./wp
+ln -s $setup_basepath/config/.htaccess-standard ./.htaccess
+ln -s $setup_basepath/config/wp-cli.yml ./wp-cli.yml
+ln -s $setup_basepath/config/wp-env.php ./wp-env.php
 
 # Copy setup files
 cp ./_setup/composer.json ./composer.json
@@ -60,12 +61,12 @@ sed \
     -e "s/SET_ENV/$setup_env/g" \
     -e "s/SET_URL/$setup_url/g" \
     -e "s/SET_DBNAME/$setup_dbname/g" \
-    -e "s/SET_PATH/$setup_path/g" \
+    -e "s/SET_BASEPATH/${setup_basepath//\//\\/}/g" \
     ./config/.env.local-tmp > ./config/.env
     rm ./config/.env.local-tmp
-echo -e "Environment variable have been set"
+echo -e "Environment variables have been set"
 
-# # Build site config file
+# Build site config file
 cp ./_setup/site-config.php ./site-config-tmp.php
 sed \
     -e "s/SET_PREFIX/$setup_prefix/g" \
@@ -74,6 +75,6 @@ sed \
 echo -e "Site config file has been created"
 
 # Final clean-up
-rm setup.sh
+# rm setup.sh
 
 echo -e "Site setup completed\n"
